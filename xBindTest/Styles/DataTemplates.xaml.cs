695625b259879e05d348ea1e1bc44b54a614b9a1
imnbwd <nbwd@qq.com>
2017-04-06 12:53:41 +0800

    add the demo how to use datatemplate in resourcedictionary

---
+++ xBindTest/Styles/DataTemplates.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace xBindTest.Styles
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DataTemplates : ResourceDictionary
+    {
+        public DataTemplates()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
